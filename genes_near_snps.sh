a726b93d9b39a2fe7adf9246e309eeda0798f2e0
Beth Moore <110459600+aphidbeth@users.noreply.github.com>
2025-02-04 11:28:20 +0000

    Update to use snpeff instead

--- genes_near_snps.sh
+++ genes_near_snps.sh
@@ -1,33 +1,58 @@
-!/bin/bash
-
 # Notes on annotation of snps 
 
-# download the gff file from figshare (https://figshare.com/articles/dataset/Sitobion_avenae_genome_annotation_alternate_/14675394?file=28177590)
+# run in an interactive session
 
-# Create conda environment with bedtools and bedops: 
+# Resources required: 
+# Sitobion avenae genome (https://figshare.com/articles/dataset/Sitobion_avenae_genome_sequence_primary_/14675367?file=28177452)
+# The gff file for this genome from figshare (https://figshare.com/articles/dataset/Sitobion_avenae_genome_annotation_primary_/14675391?file=28177587)
+# Protein sequences for this genomes from figshare (https://figshare.com/articles/dataset/Sitobion_avenae_proteins_primary_/14675418?backTo=%2Fcollections%2FGrain_aphid_Sitobion_avenae_genomics%2F5425896&file=28177614)
+# snpEff for annotation : https://pcingola.github.io/SnpEff/
+# gffread for creating a CDS file (download using conda below)
+# Java for running snpEff  (download using conda below)
 
-conda create -n bedtools 
+# Create conda environment with java: 
 
-conda install bedtools bedops -n bedtools
+conda create -n java
 
-# Convert the gff to a bed file using gff2bed
+conda install java -n java
 
-gff2bed <  S.avenae-alternate-V1.0.braker.gff3 > GENES.bed
+conda install gffread -n gffread
 
-# Get the chromosome and co-ordinates of our putative adaptive snps (uploaded output from R)
+source activate java
 
-head SNPs.bed
+# Create the files and folders we need: 
 
-# Look at the closest genes to the these snps: 
+cd snpEff
 
-closest-features --closest --dist < SNPs.bed GENES.bed > genes-closest-to-snps.bed
+mkdir data/sa_v1.0
 
-# Filter out any that are more than 10000 up/downstream of snps:
+mv S.avenae-primary-V1.0.braker.gff3 data/sa_v1.0/genes.gff
+mv S.avenae-primary-V1.0.proteins.fasta data/sa_v1.0/proteins.fa
+mv S.avenae-primary-V1.0.fna data/sa_v1.0/sequences.fa
+ 
+# create CDS file
 
- awk -F "|" -v max_distance=10000 '{ \
-    if ($3 <= max_distance) { \
-        print $0; \
-    } \
-}'  genes-closest-to-snps.bed >  genes-closest-to-snps-filtered.bed
+gffread -g sequences.fa -x cds.fa genes.gff
 
-# Visualise the results
+# Compress files: 
+gzip genes.gff
+gzip sequences.fa
+gzip proteins.fa
+gzip cds.fa
+
+# Build custom database with snpEff: 
+
+# add a line to the config file under # Non-standard Databases manually using nano 
+# sa_V1.0.genome : sitobion_avenae
+
+
+# Build the custom database
+
+java -jar snpEff.jar build -gff3 -v sa_v1.0
+
+# this should put a bunch of .bin files in your data/sa_v1.0 folder
+
+
+# Run again with your vcf file to find out where the variants lie
+
+java -jar snpEff.jar -v -i bed -o bedAnn sa_V1.0 all.SNPs.bed > test.ALL.SNPS.ann.bed # note this needs updating to VCF when I get home.
